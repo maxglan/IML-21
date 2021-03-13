b193a81b0703c46039211a0cf76cc201bfa16d59
grangeorg <engin.georg@gmail.com>
2021-03-13 14:23:50 +0100

    added Monte Carlo (randomness)

--- Task 1a/main.py
+++ Task 1a/main.py
@@ -13,15 +13,21 @@ length = len(l)
 # 10 folds
 k = 10
 
+np.random.seed(42)
+np.random.shuffle(train)
+
+# extracting the training data and the results
+X = train[:, 1:]
+y = train[:, 0]
+
+
 # array in which the solutions are stored
 solution = np.zeros(length)
 
 for i,j in zip(l, range(length)):
 # performing k fold validation for each lambda
     model = Ridge(i)
-    # extracting the training data and the results
-    X = train[:, 1:]
-    y = train[:, 0]
+
     # T returns the array of scores of the estimator for each run of the cross validation
     T = cross_val_score(model, X, y, scoring='neg_root_mean_squared_error', cv=k)
     print(T)
