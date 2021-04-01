33cd0c3db215de66ef6d7449bfa46b12b8d1b82a
grangeorg <engin.georg@gmail.com>
2021-04-01 13:35:46 +0200

    Update main.py
    better comments

--- Task 1b/main.py
+++ Task 1b/main.py
@@ -3,7 +3,6 @@
 
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import cross_val_score
 
 # read the csv file
 train = np.loadtxt(open('train.csv', "rb"), delimiter=",", skiprows=1)
@@ -14,38 +13,25 @@ X = train[:, 2:]
 y = train[:, 1]
 
 
-#calculating our new basis
+#function for calculating our new basis
 def features(x):
     f= [x,x**2, np.exp(x), np.cos(x)]
     f1= np.reshape(f, -1)
     f1= np.append(f1, 1)
     return f1
 
-#calculating the values in our basis
+#creating array for our basis& filling it
 phi=np.zeros((700, 21))
 
 for i in range(len(y)):
     phi[i]= features(X[i])
 
 
+#fit_intercept=False as we have implemented the constant offset of our linear function as the 21st entry of phi
 model = LinearRegression(fit_intercept=False).fit(phi, y)
-
-#fit_intercept=False
 
 #saving the solution
 solution= model.coef_
 
 # export to csv
 np.savetxt("solution.csv", solution)
-  
-
-# for i,j in zip(l, range(length)):
-# # performing k fold validation for each lambda
-#     model = Ridge(i)
-
-#     # T returns the array of scores of the estimator for each run of the cross validation
-#     T = cross_val_score(model, X, y, scoring='neg_root_mean_squared_error', cv=k)
-#     # takes the mean of the RMSEs for each lambda and stores it in solutions
-#     solution[j] = 0.1 * sum(-T)
-
-
