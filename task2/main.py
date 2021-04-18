30a219c9c0e728f85d3adab46ebb84566ed8e060
Max <maxgl@student.ethz.ch>
2021-04-18 14:38:33 +0200

    Using numpy instead of panda for preprocessing

--- task2/main.py
+++ task2/main.py
@@ -7,6 +7,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from numba import njit
 
 # read the csv file into a Panda df
 #train = np.loadtxt(open('train.csv', "rb"), delimiter=",", skiprows=1)
@@ -29,27 +30,42 @@ features = list(trainf.columns)
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
+trainf_arr = trainf.to_numpy(float, True, 0.)
 
-#here we attempt to deal with the missing data points (NaNs)
-for i in id:
-    for f in features:
-        
-        #returns indices of rows for a specific patient id
-        values = trainf.index[trainf['pid'] == i  ].tolist()
-        
-        #check whether all entries of a specific feature of a patient are NaNs
-        
-        #if yes: replace all with 0
-        if np.all( np.isnan( trainf[f][values] ) ) == True:
-            trainf[f][values] = 0
+#@njit
+def trying_numba(trainf, num_ids, num_feat):
+    #here we attempt to deal with the missing data points (NaNs)
+    for i in range(num_ids):
+        for f in range(num_feat):
             
-        #else if any entry is a nan eg local minimum
-        elif np.any( np.isnan( trainf[f][values] ) ) == True:
-            minimum = min( trainf[f][values] )
-        
-            #check wether specific entry is nan and then replace it with minimum
-            for v in values:
-                if np.isnan(trainf[f][v]) == True:
-                    trainf[f][v] = minimum
+            #check whether all entries of a specific feature of a patient are NaNs
             
+            #if yes: replace all with 0
+            if np.all( np.isnan( trainf[i:i+12,f] ) ) == True:
+                trainf[i:i+12,f] = 0
+                
+            #else if any entry is a nan eg local minimum
+            elif np.any( np.isnan( trainf[i:i+12,f] ) ) == True:
+                minimum = np.nanmin( trainf[i:i+12,f] )
             
+                #check wether specific entry is nan and then replace it with minimum
+                for v in range(len(trainf[i:i+12,f])):
+                    if np.isnan( trainf[i+v,f] ) == True:
+                        trainf[i+v,f] = minimum
+        i = i + 12
+                    
+    return trainf
+
+
+reshaped_arr = np.zeros((len(id), 37*12))
+
+hallo = np.reshape(trainf_arr[0:0+12, :], (-1,))
+
+for i, id_i in enumerate(np.arange(0, len(id), 12)):
+    reshaped_arr[i,:] = np.reshape(trainf_arr[id_i:id_i+12, :], (-1,), order = 'F')
+    
+#get rid of the multiple patien IDs:
+reshaped_arr = reshaped_arr[:, 11:]
+
+
+#trainf_arr = trying_numba(trainf_arr, len(id), len(features))
