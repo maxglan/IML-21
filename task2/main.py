060f6201248c178b7320e43ba09eb7186acd37dd
Max <maxgl@student.ethz.ch>
2021-04-18 16:33:46 +0200

    Cleaned up main.py | Added better comments

--- task2/main.py
+++ task2/main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-@author: georgengin
-"""
 
 """ Imports """
 
@@ -27,56 +24,65 @@ id = trainf.pid.unique()
 #list with names of the features
 features = list(trainf.columns)
 
-pd.options.mode.chained_assignment = None  # default='warn'
-
-trainf_arr = trainf.to_numpy(float, True, 0.)
-
+#Creates a numpy array out of the pd dataframe
+trainf_arr = trainf.to_numpy(float, True)
 
 """  Deal with missing data points """
-#@njit
-def trying_numba(trainf, num_ids, num_feat):
-    for i in range(num_ids):
+@njit
+def deal_with_nans(t_arr, num_ids, num_feat):
+    """
+    Parameters
+    ----------
+    t_arr : ndarray
+        contains the training data
+    num_ids : int64
+        number of patient ids
+    num_feat : TYPE
+        number of features (== columns of t_arr)
+
+    Returns
+    -------
+    Nothing, t_arr is call-by-reference
+    
+    If all the data of a patient's feature is missing, we set all the value to zero.
+    If only some data is missing, we set the nans to the minimum of that patient's
+    feature.
+    """
+    for i in np.arange(0, num_ids*12, 12):
         for f in range(num_feat):
-            
             #check whether all entries of a specific feature of a patient are NaNs
             
             #if yes: replace all with 0
-            if np.all( np.isnan( trainf[i:i+12,f] ) ) == True:
-                trainf[i:i+12,f] = 0
+            if np.all( np.isnan( t_arr[i:i+12,f] ) ) == True:
+                t_arr[i:i+12,f] = 0
                 
             #else if any entry is a nan eg local minimum
-            elif np.any( np.isnan( trainf[i:i+12,f] ) ) == True:
-                minimum = np.nanmin( trainf[i:i+12,f] )
+            elif np.any( np.isnan( t_arr[i:i+12,f] ) ) == True:
+                minimum = np.nanmin( t_arr[i:i+12,f] )
             
                 #check wether specific entry is nan and then replace it with minimum
-                for v in range(trainf[i:i+12,f].shape[0]):
-                    if np.isnan( trainf[i+v,f] ) == True:
-                        trainf[i+v,f] = minimum
-        #i = i + 12
-                    
-    return trainf
-
+                for v in range(12):
+                    if np.isnan( t_arr[i+v,f] ) == True:
+                        t_arr[i+v,f] = minimum
+                
+#no return value because call by reference
+deal_with_nans(trainf_arr, len(id), len(features))
 
 """ Reshaping to use in SVM """
-reshaped_arr = np.zeros((len(id), 37*12))
-
-hallo = np.reshape(trainf_arr[0:0+12, :], (-1,))
+trainf_arr_reshaped = np.zeros((len(id), 37*12))
 
 for i, id_i in enumerate(np.arange(0, len(id), 12)):
-    reshaped_arr[i,:] = np.reshape(trainf_arr[id_i:id_i+12, :], (-1,), order = 'F')
+    trainf_arr_reshaped[i,:] = np.reshape(trainf_arr[id_i:id_i+12, :], (-1,), order = 'F')
     
-#get rid of the multiple patien IDs:
-reshaped_arr = reshaped_arr[:, 11:]
-
-
-trainf_arr = trying_numba(trainf_arr, len(id), len(features))
+#get rid of multiple patient IDs:
+trainf_arr_reshaped = trainf_arr_reshaped[:, 11:]
 
 
 """ Subtasks """
 
-subtask1(trainf=trainf_arr, trainl=trainl)
-subtask2(trainf=trainf_arr, trainl=trainl)
-subtask3(trainf=trainf_arr, trainl=trainl)
+#subtask1(trainf=trainf_arr, trainl=trainl)
+#subtask2(trainf=trainf_arr, trainl=trainl)
+#subtask3(trainf=trainf_arr, trainl=trainl)
 
 
 
