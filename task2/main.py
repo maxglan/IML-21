55cce2053ef93b1da54cb28829fa87135bdcce87
grangeorg <engin.georg@gmail.com>
2021-04-20 10:36:10 +0200

    4x36 features

--- task2/main.py
+++ task2/main.py
@@ -68,6 +68,45 @@ def deal_with_nans(t_arr, num_ids, num_feat):
     
     return t
       
+def deal_with_nans_badly(t_arr, num_ids, num_feat):
+    """ Returns the preprocessed and reshaped array """
+    
+    print(" Deal with missing data.")
+    
+    #Creates a numpy array out of the pd dataframe
+    arr = t_arr.to_numpy(float, True)
+    t_reshaped = np.zeros((num_ids, 37*4))
+    
+    for i,j in zip(np.arange(0, num_ids*12, 12), range(num_ids)):
+        for f in range(num_feat):
+            #check whether all entries of a specific feature of a patient are NaNs
+            
+            a=arr[i:i+12,f]
+            #if yes: replace all with 0
+            if np.all( np.isnan( a ) ) == True:
+                a = 0
+                t_reshaped[j,f*2 :f*2 +3]=0
+                
+            #else if any entry is a nan eg local minimum
+            elif np.any( np.isnan( a ) ) == True:
+
+                mean = np.nanmean( a )
+                minimum = np.nanmin( a )
+                maximum = np.nanmax( a )
+                
+                start = a[np.isfinite(a)][0]
+                end = a[np.isfinite(a)][-1]          
+                trend = (start + end)*0.5
+                
+                t_reshaped[j,f*2] = mean
+                t_reshaped[j,f*2 +1] = trend
+                t_reshaped[j,f*2 +2] = minimum
+                t_reshaped[j,f*2 +3] = maximum
+        
+    #get rid of multiple patient IDs:
+    t = t_reshaped[:, 1:]
+    
+    return t
 
 """ Normalize the data """
 # If we use non-linear SVM we first have to normalize the data using 
@@ -93,6 +132,7 @@ def normalize_combined(train_features, test_features):
     return norm_train_features, norm_test_features
     
 
+"""calculating nans"""
 #returns properly reshaped and filled arrays
 train_features = deal_with_nans(trainf, len(idtrain), len(features))
 test_features = deal_with_nans(testf, len(idtest), len(features))
@@ -101,13 +141,29 @@ test_features = deal_with_nans(testf, len(idtest), len(features))
 norm_train_features, norm_test_features = normalize_combined(train_features, test_features)
 
 
+#returns badly reshaped and filled arrays
+train_features_bad = deal_with_nans(trainf, len(idtrain), len(features))
+test_features_bad = deal_with_nans(testf, len(idtest), len(features))
+
+# normalised bad versions
+norm_train_features_bad, norm_test_features_bad = normalize_combined(train_features_bad, test_features_bad)
+
 """ Subtasks """
 
-# prediction1 = subtask1(train_features , trainl, test_features )
-prediction1 = subtask1(norm_train_features , trainl, norm_test_features )
+# # prediction1 = subtask1(train_features , trainl, test_features )
+# prediction1 = subtask1(norm_train_features , trainl, norm_test_features )
 
-# prediction2 = subtask2(train_features , trainl, test_features )
-prediction2 = subtask2(norm_train_features , trainl, norm_test_features )
+# #prediction2 = subtask2(train_features , trainl, test_features )
+# prediction2 = subtask2(norm_train_features , trainl, norm_test_features )
+
+# prediction3 = subtask3(train_features , trainl, test_features )
+
+"""bad subtasks"""
+# prediction1 = subtask1(train_features , trainl, test_features )
+prediction1 = subtask1(norm_train_features_bad , trainl, norm_test_features_bad )
+
+#prediction2 = subtask2(train_features , trainl, test_features )
+prediction2 = subtask2(norm_train_features_bad , trainl, norm_test_features_bad )
 
 prediction3 = subtask3(train_features , trainl, test_features )
 
