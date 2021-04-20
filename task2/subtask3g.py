f11964c922f74595aad4fb6ce4ec56b0f464c5e4
grangeorg <engin.georg@gmail.com>
2021-04-20 16:04:01 +0200

    Create subtask3g.py

---
+++ task2/subtask3g.py
@@ -0,0 +1,32 @@
+
+""" Imports """
+
+import numpy as np
+from sklearn import svm
+import pandas as pd
+
+""" Functions """
+
+def subtask3g(trainf, trainl, test): 
+    """
+    returns the predicted data using support vector regression
+    """
+    
+    print(" Start subtask 3.")
+
+    label= ["LABEL_RRate", "LABEL_ABPm", "LABEL_SpO2", "LABEL_Heartrate"]
+    y={}
+    model={}
+
+    prediction = np.zeros((12664, 4))
+
+    # performing 10 fold cross validation for 5 different lambdas on each of the 4 labels
+    for i, j in zip(label, range(4)):
+    
+        y[i] = trainl[i]
+        model[i] = svm.SVR(kernel= 'rbf')
+        model[i].fit(trainf , y[i])
+        
+        prediction[:, j] = model[i].predict(test)
+    
+    return prediction
