b38e560494bac25f7dd8db9b7a7eae8a2a5a0750
grangeorg <engin.georg@gmail.com>
2021-05-03 14:00:02 +0200

    new mechanism for features in separate file

---
+++ task3/charto27int.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  3 13:32:18 2021
+
+@author: georgengin
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+from numba import njit
+
+""" Read the csv file """
+
+print("Read CSV files")
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+sample = np.loadtxt("sample.csv")
+
+#this gives me just the activity label of the training set
+active = train.iloc[:,1]
+
+"""create a dataframe with 4 entries in each row (one for each Amino Acid), 
+where we convert the char of an acid into an int via the function ord"""
+
+def chartoint(df):
+    length = len(df.index)
+    Sequence = np.zeros((length, 26*4))
+    
+    for i in range(length):
+        l = list(df.iloc[i,0])
+        
+        number = [ord(k)-64 for k in l]
+        
+        for j, m in enumerate(number, len(number)):
+            Sequence[i, j +26*m] += 1
+        
+    return Sequence
--- task3/main.py
+++ task3/main.py
@@ -38,7 +38,7 @@ test_sequence = chartoint(test)
 
 """training our Neural Network"""
 print("train NN")
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, max_iter= 6900, random_state=1)
+clf = MLPClassifier(hidden_layer_sizes = (1690, 690, 69), max_iter= 6900, verbose=True)
 clf.fit(train_sequence, active)
 
 solution = clf.predict(test_sequence)
